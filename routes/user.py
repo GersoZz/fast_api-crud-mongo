0ec6ce5f121241e2720934fc9c4c172f2c91d99f
GersoZz <gersonzuniga@uni.pe>
2024-10-19 16:12:06 -0500

    feat: add delete user endpoint

--- routes/user.py
+++ routes/user.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from config.db import conn
 from schemas.user import userEntity, usersEntity
 from models.user import User
 from passlib.hash import sha256_crypt
 from bson import ObjectId
+from starlette.status import HTTP_204_NO_CONTENT
 
 user = APIRouter()
 
@@ -32,5 +33,6 @@ def update_user():
   return "Hello World"
 
 @user.delete('/users/{id}')
-def delete_user():
-  return "Hello World"
+def delete_user(id: str):
+  conn.local.user.find_one_and_delete({"_id": ObjectId(id)})
+  return Response(status_code=HTTP_204_NO_CONTENT)
